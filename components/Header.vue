a8b01bc21ecd8362016ec8f03fa3acd8f514666d
khachuy158 <khachuy.t.1998@gmail.com>
2022-03-09 09:57:32 +0700

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-breadcrumbs
+      :items="items"
+      divider="-"
+      class="breakcrumb"
+    ></v-breadcrumbs>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    items: [
+      {
+        text: 'Dashboard',
+        disabled: false,
+        href: 'breadcrumbs_dashboard',
+      },
+      {
+        text: 'Link 1',
+        disabled: false,
+        href: 'breadcrumbs_link_1',
+      },
+      {
+        text: 'Link 2',
+        disabled: true,
+        href: 'breadcrumbs_link_2',
+      },
+    ],
+  }),
+}
+</script>
+<style scoped lang="scss">
+.breakcrumb {
+  justify-content: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col md="12" justify="center" align="center">Example</v-col>
+    <v-col md="12"><Header /> </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+import Header from '~/components/Header.vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { Header },
+})
+</script>
